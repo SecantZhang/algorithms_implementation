c98bf745da4cc4515eb9f848efc508b432a77997
Wilson (Zheng) Zhang <abovesummitwz@gmail.com>
2018-08-29 11:25:42 -0400

    Updated the comments of Selection_Sort algorithm.

--- Selection_Sort/js/selection_sort.js
+++ Selection_Sort/js/selection_sort.js
@@ -1,9 +1,20 @@
+/**
+ * Swap function for swap the array elements with the first index and second index. 
+ * @param {*} array 
+ * @param {*} firstIndex 
+ * @param {*} secondIndex 
+ */
 var swap = function(array, firstIndex, secondIndex) {
     var temp = array[firstIndex];
     array[firstIndex] = array[secondIndex];
     array[secondIndex] = temp;
 };
 
+/**
+ * Function for finding the minimum value index. 
+ * @param {*} array 
+ * @param {*} startIndex 
+ */
 var indexOfMinimum = function(array, startIndex) {
 
     var minValue = array[startIndex];
@@ -18,6 +29,9 @@ var indexOfMinimum = function(array, startIndex) {
     return minIndex;
 }; 
 
+/**
+ * Selection Sort Algorithm Implementation. 
+ */
 var selectionSort = function(array) {
     var minIndex;
     for (var i = 0; i < array.length - 1; i++) {
@@ -26,12 +40,17 @@ var selectionSort = function(array) {
     }
 };
 
+/**
+ * Examples using selection sort. 
+ */
+
 var array = [22, 11, 99, 88, 9, 7, 42];
 selectionSort(array);
 println("Array after sorting:  " + array);
-
 Program.assertEqual(array, [7, 9, 11, 22, 42, 88, 99]);
+
 var array2 = [-88, -99, -77, -93, -101, -144];
 selectionSort(array2);
-Program.assertEqual(array2, [-144, -101, -99, -93, -88, -77]);
 println("Array2 after sorting: " + array2);
+Program.assertEqual(array2, [-144, -101, -99, -93, -88, -77]);
+
