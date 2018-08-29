bd3c60a780210d7c548ff692ebcb1f2d6bcc9d13
Wilson (Zheng) Zhang <abovesummitwz@gmail.com>
2018-08-29 11:22:13 -0400

    Added the algorithm for selection sort using javascript

---
+++ Selection_Sort/js/selection_sort.js
@@ -0,0 +1,37 @@
+var swap = function(array, firstIndex, secondIndex) {
+    var temp = array[firstIndex];
+    array[firstIndex] = array[secondIndex];
+    array[secondIndex] = temp;
+};
+
+var indexOfMinimum = function(array, startIndex) {
+
+    var minValue = array[startIndex];
+    var minIndex = startIndex;
+
+    for(var i = minIndex + 1; i < array.length; i++) {
+        if(array[i] < minValue) {
+            minIndex = i;
+            minValue = array[i];
+        }
+    } 
+    return minIndex;
+}; 
+
+var selectionSort = function(array) {
+    var minIndex;
+    for (var i = 0; i < array.length - 1; i++) {
+        minIndex = indexOfMinimum(array, i);
+        swap(array, i, minIndex);
+    }
+};
+
+var array = [22, 11, 99, 88, 9, 7, 42];
+selectionSort(array);
+println("Array after sorting:  " + array);
+
+Program.assertEqual(array, [7, 9, 11, 22, 42, 88, 99]);
+var array2 = [-88, -99, -77, -93, -101, -144];
+selectionSort(array2);
+Program.assertEqual(array2, [-144, -101, -99, -93, -88, -77]);
+println("Array2 after sorting: " + array2);
